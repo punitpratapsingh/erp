fd01fdb05e28c24671e8d711b57206f09aaccf59
punitpratapsingh <punitpratapsingh30@gmail.com>
2020-03-02 15:50:12 +0530

    env file

--- server/constants.js
+++ server/constants.js
@@ -20,24 +20,10 @@ export const {
 
 const db = process.env.MONGO_DB || erp;
 
-/**
- * @description
- * This is the sample constact specifier for queues
- * The queue names follow follow the "camelcase" naming
- * convention wehere the first letter of the queue will
- * be capital case. The queue channels are defined under server/queues/
- * directory and will be autoloded by directory indexer unless explicitly
- * ignored in skip array in index.js. The sampleQueue.js is a sample
- * channel that is meant to be updated/renamed as per the queue requirements.
- * To know more about the channel convention and design principles
- * @contact sharma02gaurav@gmail.com
- */
-export const AMQP_QUEUES = {
-	SAMPLE_QUEUE: 'SampleQueue',
-};
+
 
 // export const mongoConnectionString = `mongodb://${host}:${port}/${db}`;
-export const mongoConnectionString = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@usermicroservice-5jre5.mongodb.net/${db}?retryWrites=true`;
+export const mongoConnectionString = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@erp-v0la5.mongodb.net/test?retryWrites=true&w=majority`;
 
 // this string is unique for each project construction
 export const secretString = SECRET_STRING;
